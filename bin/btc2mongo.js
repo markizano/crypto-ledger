4a08c85de4b28b73dedcc713735348da77df5240
Markizano Draconus <markizano@markizano.net>
2022-06-17 21:01:17 -0500

    Lets log something more meaningful in the top-level error handler, including the stacktrace to know what failed.

--- bin/btc2mongo.js
+++ bin/btc2mongo.js
@@ -2,12 +2,16 @@
 
 const utils = require('../lib/cryptoview/utils');
 const btc2mongo = require('../lib/cryptoview/btc2mongo');
+const { log } = require('../lib/cryptoview/logger');
+const __name__ = 'btc2mongo.js';
 
 // Python equivalent of if __name__ == "__main__":
 if ( typeof require !== "undefined" && require.main == module ) {
     btc2mongo.main()
       .then(utils.debugPass)
-      .catch(utils.debugFail)
+      .catch((err) => {
+        log.error(__name__, `[ \x1b[1;31mException\x1b[0m ]: Top-Level Error: ${err}\n${err.stack}`)
+      })
       .finally(btc2mongo.tidyUp);
 }
   
