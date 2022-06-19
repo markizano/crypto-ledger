7b0ae3b8cd023882b8a38dda30ceb6d6aa63dde6
Markizano Draconus <markizano@markizano.net>
2022-06-19 00:29:43 -0500

    Adding 2 more stub adapters for getting in front of config for Bitcoin-like adapters that might have additional functionality I may want to implement, like for Ravencoin coming within the next few commits.

---
+++ lib/cryptoview/adapter/doge.ts
@@ -0,0 +1,5 @@
+
+import { BitcoinRpcClientConfig } from "cryptoview/adapter/btc";
+
+// noop: Just emulate bitcoin until I can develop on this for differences.
+export class DogecoinRpcClientConfig extends BitcoinRpcClientConfig {}
